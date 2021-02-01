96b861fd6e8e6fe920b46857c75e70e3c627eb46
Mr-OmarFaruk <fcifaruk@gmail.com>
2021-02-01 19:59:53 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,51 @@
+function getPin(){
+    const random=Math.random()*10000;
+    const pin=(random + '').split('.')[0];
+    if(pin.length === 4){
+        return pin;
+    }
+    else{
+        return getPin;
+    }
+} 
+// display generated pin
+function generatePin(){
+    const pinInput=document.getElementById('pin');
+    pinInput.value=getPin();
+}
+// handle calc btn event
+const buttonContainer=document.getElementById('digits-container');
+buttonContainer.addEventListener('click', function(event){
+    const digit=event.target.innerText;
+    if(isNaN(digit)){
+        //handle backspace
+        //handle clear
+        if(digit === 'C'){
+            const typedInput=document.getElementById('typed-pin');
+            typedInput.value=''; 
+        }
+    }
+    else{
+        const typedInput=document.getElementById('typed-pin');
+        typedInput.value=typedInput.value+digit;
+    }
+});
+
+//verify pin
+function verifyPin(){
+    const pin=document.getElementById('pin').value;
+    const typedPin=document.getElementById('typed-pin').value;
+    if(pin === typedPin){
+        displayMatchResesult('block','none');
+    }//pin matched showing
+    else{
+        displayMatchResesult('none','block');
+    }//pin didnot match showing
+}
+
+function displayMatchResesult(correctStatus, incorrectStatus){
+    const correct=document.getElementById('correct-pin');
+    correct.style.display=correctStatus;
+    const incorrect=document.getElementById('incorrect-pin');
+    incorrect.style.display=incorrectStatus;
+}
